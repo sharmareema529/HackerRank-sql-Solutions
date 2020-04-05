982c734a854c6560705c78b05f67cc09bc03c913
sharmareema529 <62931400+sharmareema529@users.noreply.github.com>
2020-04-06 00:41:31 +0530

    Create SymmetricPairs.sql

---
+++ Sql-Programs/SymmetricPairs.sql
@@ -0,0 +1,1 @@
+Select distinct a.x as w,a.y from functions a ,functions b Where a.x =b.y and b.x = a.y and a.x < b.x Union all Select a1.x as w,a1.y from functions a1 where a1.x = a1.y group by a1.x,a1.y having count(*)>1 Order by w;
